c0d24cd338a167b85aa51fb65085d5986352b684
cheehoo1 <artemgrishin@gmail.com>
2020-03-23 17:28:11 -0400

    section4

---
+++ section4/section4.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int fav_num;
+    std::cout << "Enter your favorite number between 1 and 100: ";
+    std::cin >> fav_num;
+    std::cout << "Amazing!! " << fav_num << " is my favorite number too!" << std::endl;
+    return 0;
+}
